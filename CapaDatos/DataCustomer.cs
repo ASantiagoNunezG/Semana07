b3f007cad6d57d2962fc9e1660f5be4ac10bf479
A. Santiago Nunez <abraham.nunez@tecsup.edu.pe>
2025-05-08 20:11:40 -0500

    Agregar archivos de proyecto.

---
+++ CapaDatos/DataCustomer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class DataCustomer
+    {
+        private string connectionString = "Data Source=LAPTOP-R79EK4NG\\SQLEXPRESS2017;Initial Catalog=Semana07;User ID =user322; Password =DOTA2wtf; TrustServerCertificate=True; Encrypt=True";
+
+        public List<Customer> GetAll()
+        {
+            List<Customer> list = new List<Customer>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("USP_listar_customers", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    list.Add(new Customer
+                    {
+                        CustomerId = (int)reader["customer_id"],
+                        Name = reader["name"].ToString(),
+                        Address = reader["address"].ToString(),
+                        Phone = reader["phone"].ToString(),
+                        Active = (bool)reader["active"]
+                    });
+                }
+            }
+
+            return list;
+        }
+
+        public void InsertCustomer(Customer customer)
+        {
+           
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("USP_insert_customer", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@name", customer.Name);
+                cmd.Parameters.AddWithValue("@address", customer.Address);
+                cmd.Parameters.AddWithValue("@phone", customer.Phone);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        
+
+        }
+    }
+}
---
+++ Semana07/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CapaDatos;
+using CapaNegocio;
+
+namespace Semana07
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window    
+    {
+        private BusinessCustomer customerService = new BusinessCustomer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            CargarClientes();
+        }
+        private void CargarClientes()
+        {
+            var lista = customerService.ObtenerTodos();
+            dgClientes.ItemsSource = lista; // dgClientes es tu DataGrid en XAML
+        }
+        private void BtnBuscar_Click(object sender, RoutedEventArgs e)
+        {
+            string nombre = txtBuscar.Text.Trim();
+            var resultado = customerService.BuscarClientesPorNombre(nombre);
+            dgClientes.ItemsSource = resultado;
+        }
+        private void BtnRegistrar_Click(object sender, RoutedEventArgs e)
+        {
+            Customer c = new Customer
+            {
+                Name = txtName.Text,
+                Address = txtAddress.Text,
+                Phone = txtPhone.Text,
+            };
+
+            try
+            {
+                customerService.RegistrarCustomer(c);
+                MessageBox.Show("Cliente registrado correctamente");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al registrar: " + ex.Message);
+            }
+        }
+    }
+}
